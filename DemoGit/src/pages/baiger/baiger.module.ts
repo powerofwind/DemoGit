25320e20e2f59891e5aa433ffcccb66efde4b613
sakchai17389 <ball17389@hotmail.com>
2019-02-26 13:17:47 +0700

    update

---
+++ DemoGit/src/pages/baiger/baiger.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BaigerPage } from './baiger';
+
+@NgModule({
+  declarations: [
+    BaigerPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BaigerPage),
+  ],
+})
+export class BaigerPageModule {}
